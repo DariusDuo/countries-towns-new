ee31a906c89fdb2d712d145ed2bb7aae95a4d789
Darius Duoba <darius.duoba@gmail.com>
2021-07-23 16:59:04 +0300

    connected to mongoose

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// prisijungimas prie duomenu bazes
+mongoose
+  .connect(process.env.MONGO_CONNECT_STIRNG, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Conneced to Mongo ooooooooose');
+  })
+  .catch((err) => console.error(err.message));
 
 //MiddleWare
 app.use(morgan('dev'));
@@ -13,4 +28,4 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(PORT, console.log(`server is running on port ${PORT}`));
