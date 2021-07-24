9091247fbd6297504b861e21e6168d49192d9724
Darius Duoba <darius.duoba@gmail.com>
2021-07-24 18:29:53 +0300

    model places schema, PORT=4000 to .env

--- server/routes/PlacesRoutes.js
+++ server/routes/PlacesRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const PlaceModel = require('../models/Place');
 const router = express.Router();
 
 //create new place
 router.post('/api/place/new', (req, res) => {
-  console.log('you are going to create a place');
+  const gotNewPlaceData = req.body;
+  console.log('gotNewPlaceData', gotNewPlaceData);
+  res.json('you are about to create a newplace');
 });
 
 module.exports = router;
